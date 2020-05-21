caee1dd02b7701772ce164487cdfde53e2ef82ee
Alessandro Domanico <alessandro.domanico@yahoo.it>
2020-05-22 00:16:46 +0200

    Added central

--- my-map-in-flask.py
+++ my-map-in-flask.py
@@ -48,9 +48,9 @@ def query(dateFrom=None, dateTo=None):
     createDBconnection()
     # execute default query on the DB
     if not dateFrom:
-        dateFrom = '2019-01-01'
+        dateFrom = '2020-05-01'
     if not dateTo:
-        dateTo = '2020-12-31'
+        dateTo = '2020-05-01'
     default_query = "SELECT OPD_ID, OPD_DATE_VIS, OPD_DIS_ID_A, DIS_DESC, PAT_CITY, LOC_CITY, PAT_ADDR, LOC_ADDRESS, LOC_LAT, LOC_LONG FROM OPD \
                         LEFT JOIN PATIENT ON PAT_ID = OPD_PAT_ID \
                         LEFT JOIN DISEASE ON DIS_ID_A = OPD_DIS_ID_A \
@@ -68,19 +68,20 @@ def query_group(dateFrom=None, dateTo=None):
     createDBconnection()
     # execute default query on the DB
     if not dateFrom:
-        dateFrom = '2019-01-01'
+        dateFrom = '2020-01-01'
     if not dateTo:
-        dateTo = '2019-01-31'
+        dateTo = '2020-05-01'
     default_query = "SELECT COUNT(*) AS COUNT, INTERNAL.* FROM( \
                         SELECT OPD_ID, OPD_DATE_VIS, OPD_DIS_ID_A, DIS_DESC, PAT_CITY, LOC_CITY, PAT_ADDR, LOC_ADDRESS, LOC_LAT, LOC_LONG FROM OPD \
                         LEFT JOIN PATIENT ON PAT_ID = OPD_PAT_ID \
                         LEFT JOIN DISEASE ON DIS_ID_A = OPD_DIS_ID_A \
                         LEFT JOIN LOCATION ON(PAT_CITY = LOC_CITY AND PAT_ADDR = LOC_ADDRESS) \
-                        WHERE LOC_CITY IN('Wonchi', 'Wolisso Rural','Wolisso Town','Goro') \
+                        WHERE LOC_CITY IN ('Wonchi', 'Wolisso Rural','Wolisso Town','Goro') \
                         AND OPD_DATE_VIS BETWEEN '%s' AND '%s' \
                     ) INTERNAL \
                     GROUP BY OPD_DIS_ID_A, PAT_CITY, PAT_ADDR \
                     ORDER BY COUNT DESC" %(escape(dateFrom), escape(dateTo))
+    #print(default_query)                
     cursor = db.cursor(MySQLdb.cursors.DictCursor)
     cursor.execute(default_query)
     result = cursor.fetchall()
