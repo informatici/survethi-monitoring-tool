c6e5f12f8532f48aa4ec961898623f3127110cad
Alessandro Domanico <alessandro.domanico@yahoo.it>
2020-06-18 21:47:39 +0200

    Added testing env handling

--- my-map-in-flask.py
+++ my-map-in-flask.py
@@ -12,8 +12,11 @@ import os.path
 app = Flask(__name__)
 if app.config["ENV"] == "production":
     app.config.from_object("config.ProductionConfig")
+elif app.config["ENV"] == "testing":
+    app.config.from_object("config.TestingConfig")
 else:
     app.config.from_object("config.DevelopmentConfig")
+print(app.config["ENV"])
 CORS(app)
 
 db = MySQLdb
