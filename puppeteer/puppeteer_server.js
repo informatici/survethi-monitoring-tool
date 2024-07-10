ced46233c332a0732eea207e9ccc71ec99f49263
mwithi <alessandro.domanico@yahoo.it>
2024-07-10 09:08:30 +0200

    reorganize code

--- puppeteer/puppeteer_server.js
+++ puppeteer/puppeteer_server.js
@@ -12,9 +12,10 @@ require('dotenv').config();
 
 const app = express();
 const port = 3000;
-const logLevel = process.env.PUPPETEER_LOGLEVEL
-const templatePath = process.env.PUPPETEER_TEMPLATE + '.html';
-const disease_filter = ""
+const logLevel = process.env.PUPPETEER_LOGLEVEL || "info"
+const templatePath = process.env.PUPPETEER_TEMPLATE + '.html' || "pdf_template.html"
+const disease_filter = process.env.REPORT_DISEASE_FILTER || ""
+const temporal_filter = process.env.REPORT_TEMPORAL_FILTER || ""
 
 // Function to convert an image to a base64 data URL
 function imageToBase64(imagePath) {
@@ -68,130 +69,39 @@ async function generatePDFWithInteractions(url, outputPath) {
         logger.info(`Accessing: ${url}`);
         await page.goto(url, { waitUntil: 'networkidle0', timeout: 60000 });
 
-        // Click the disease filters
-        if (disease_filter != "") {
-            logger.info(`Select disease filter...`);
-            await page.waitForSelector('button[data-id="main_filter"]');
-            await page.click('button[data-id="main_filter"]');
-            await page.waitForSelector('.dropdown-menu.show');
-            await page.select('select#main_filter', disease_filter);
-        }
-
-        // Click outside the dropdown to trigger the map update and wait 5 seconds
-        await page.click('body');
-        await page.evaluate(() => new Promise(resolve => setTimeout(resolve, 5000)));
-
-        // Click the week1 button
-        logger.info(`Select week1 button...`);
-        await page.waitForSelector('button#week1', { timeout: 60000 });
-        logger.info(`Clicking week1 button...`);
-        await page.click('button#week1');
-
-        // Wait for the Map loading indicator to disappear
-        logger.info(`Waiting for map to load...`);
-        await page.waitForFunction(() => {
-            const loadingIndicator = document.querySelector('#map-loading-indicator');
-            return loadingIndicator && !loadingIndicator.classList.contains('loading');
-        }, { timeout: 60000 });
-
-        // Delay 3 seconds
-        await page.evaluate(() => new Promise(resolve => setTimeout(resolve, 3000)));
-
-        // Hide all leaflet-control elements except the legenda
-        await page.evaluate(() => {
-            const controls = document.querySelectorAll('.leaflet-control');
-            controls.forEach(control => {
-            if (!control.classList.contains('info') || !control.classList.contains('legenda')) {
-                control.style.display = 'none';
-            }
-            });
-        });
-
-        // Delay 3 seconds
-        await page.evaluate(() => new Promise(resolve => setTimeout(resolve, 3000)));
+        // Perform interactions (disease filter, week1 button, etc.)
+        await performInteractions(page);
 
         // Wait for the table to load
         logger.info(`Waiting for table to load...`);
         await page.waitForSelector('#table_primary');
 
-        // Check if the page list dropdown exists
-        const pageListExists = await page.evaluate(() => {
-            return !!document.querySelector('.page-list .dropdown-menu');
-        });
-        logger.debug(`Table rows > 5: ${pageListExists}.`);
-
-        if (pageListExists) {
-            logger.info(`Expanding table for top 10...`);
-            // Click on the "Cases" column header twice to sort descending
-            await page.click('#table_primary thead th:nth-child(2)'); // Adjust nth-child index as needed
-            await page.click('#table_primary thead th:nth-child(2)'); // Click twice to sort descending
-        
-            // Select the dropdown menu for page size and click on 10
-            await page.click('.page-size'); 
-            await page.waitForSelector('.dropdown-menu.show .dropdown-item'); // Wait for dropdown items to appear
-            await page.click('.dropdown-menu.show .dropdown-item:nth-child(2)'); // Click on the 2nd item
-        
-            // Delay 1 seconds
-            await page.evaluate(() => new Promise(resolve => setTimeout(resolve, 1000)));
-          }
-
-        // Extract specific columns from the table
-        logger.info(`Extracting table...`);
-        const tableData = await page.evaluate(() => {
-            const tableRows = Array.from(document.querySelectorAll('#table_primary tbody tr'));
-            return tableRows.map(row => {
-            const columns = row.querySelectorAll('td');
-            return {
-                type: columns[0].textContent.trim(),        // Type
-                cases: columns[1].textContent.trim(),       // Cases
-                disease: columns[2].textContent.trim(),     // Disease
-                woredaTown: columns[3].textContent.trim(),  // Woreda/Town
-                kebele: columns[4].textContent.trim()       // Kebele
-            };
-            });
+        // Check if the table has at least one valid data row
+        const tableHasData = await page.evaluate(() => {
+            const noRecordsFound = document.querySelector('#table_primary tbody tr.no-records-found');
+            const tableRows = document.querySelectorAll('#table_primary tbody tr');
+            return !noRecordsFound && tableRows.length > 0;
         });
 
-        // Generate the HTML table with only the desired columns
-        const tableHtml = `
-            <table border="1" style="border-collapse: collapse; border: 1px solid #000; width: 100%; text-align: left;">
-            <thead>
-                <tr>
-                <th style="border: 1px solid #000; padding: 8px; text-align: left;">Type</th>
-                <th style="border: 1px solid #000; padding: 8px; text-align: left;">Woreda/Town</th>
-                <th style="border: 1px solid #000; padding: 8px; text-align: left;">Kebele</th>
-                <th style="border: 1px solid #000; padding: 8px; text-align: left;">Disease</th>
-                <th style="border: 1px solid #000; padding: 8px; text-align: right;">Cases</th>
-                </tr>
-            </thead>
-            <tbody>
-                ${tableData.map(row => `
-                <tr>
-                    <td style="border: 1px solid #000; padding: 8px; text-align: left;">${row.type}</td>
-                    <td style="border: 1px solid #000; padding: 8px; text-align: left;">${row.woredaTown}</td>
-                    <td style="border: 1px solid #000; padding: 8px; text-align: left;">${row.kebele}</td>
-                    <td style="border: 1px solid #000; padding: 8px; text-align: left;">${row.disease}</td>
-                    <td style="border: 1px solid #000; padding: 8px; text-align: right;">${row.cases}</td>
-                </tr>
-                `).join('')}
-            </tbody>
-            </table>
-        `;
+        var tableHtml = "No data."
+        if (tableHasData) {
+            // Extract table data
+            const tableData = await extractTableData(page);
+            // Generate HTML table
+            tableHtml = generateHtmlTable(tableData);
+        } else {
+            logger.info('No data available in the table.');
+        }
 
         // Extract data from the webpage
         const pageTitle = await page.title();
-        const mapElement = await page.$('#mapid');
-        const mapImagePath = path.join(__dirname, 'map.png');
-        // Take a screenshot of the map
-        await mapElement.screenshot({ path: 'map.png' });
-        // Convert the map image to a base64 data URL
-        const mapImageBase64 = fs.readFileSync(mapImagePath, { encoding: 'base64' });
-        const mapImageUrl = `<img src="data:image/png;base64,${mapImageBase64}" alt="Map">`;
+        const mapImageUrl = await extractMapImage(page);
 
         // Read the PDF template
         const htmlTemplate = fs.readFileSync(templatePath, 'utf-8');
         const baseDir = path.dirname(templatePath);
 
-        // Replace local images paths with base64 data URLs
+        // Replace local images paths (if any) with base64 data URLs
         const updatedHtmlTemplate = replaceImagePathsWithBase64(htmlTemplate, baseDir);
 
         // Replace placeholders in the template with extracted data
@@ -223,6 +133,136 @@ async function generatePDFWithInteractions(url, outputPath) {
     } finally {
         await browser.close();
     }
+}
+
+async function performInteractions(page) {
+    // Click the disease filters
+    if (disease_filter != "") {
+        logger.info(`Select disease filter ${disease_filter}...`);
+        await page.waitForSelector('button[data-id="main_filter"]');
+        await page.click('button[data-id="main_filter"]');
+        await page.waitForSelector('.dropdown-menu.show');
+        await page.select('select#main_filter', disease_filter);
+    } else {
+        logger.info(`No disease filter selected.`);
+    }
+
+    // Click outside the dropdown to trigger the map update and wait 5 seconds
+    await page.click('body');
+    await page.evaluate(() => new Promise(resolve => setTimeout(resolve, 5000)));
+
+    // Click the week1 button
+    if (temporal_filter != "") {
+        logger.info(`Select ${temporal_filter} button...`);
+        await page.waitForSelector(`button#${temporal_filter}`, { timeout: 60000 });
+        logger.info(`Clicking ${temporal_filter} button...`);
+        await page.click(`button#${temporal_filter}`);
+    }else {
+        logger.info(`No temporal filter selected.`);
+    }
+
+    // Wait for the Map loading indicator to disappear
+    logger.info(`Waiting for map to load...`);
+    await page.waitForFunction(() => {
+        const loadingIndicator = document.querySelector('#map-loading-indicator');
+        return loadingIndicator && !loadingIndicator.classList.contains('loading');
+    }, { timeout: 60000 });
+
+    // Delay 3 seconds
+    await page.evaluate(() => new Promise(resolve => setTimeout(resolve, 3000)));
+
+    // Hide all leaflet-control elements except the legenda
+    await page.evaluate(() => {
+        const controls = document.querySelectorAll('.leaflet-control');
+        controls.forEach(control => {
+        if (!control.classList.contains('info') || !control.classList.contains('legenda')) {
+            control.style.display = 'none';
+        }
+        });
+    });
+
+    // Delay 3 seconds
+    await page.evaluate(() => new Promise(resolve => setTimeout(resolve, 3000)));
+}
+
+async function extractTableData(page) {
+    // Check if the page list dropdown exists
+    const pageListDisplayed = await page.evaluate(() => {
+        const pageListElement = document.querySelector('.page-list');
+        return pageListElement && window.getComputedStyle(pageListElement).display !== 'none';
+    });
+    logger.debug(`Table rows > 5: ${pageListDisplayed}.`);
+
+    if (pageListDisplayed) {
+        logger.debug(`Expanding table for top 10...`);
+        // Click on the "Cases" column header twice to sort descending
+        await page.click('#table_primary thead th:nth-child(2)'); // Adjust nth-child index as needed
+        await page.click('#table_primary thead th:nth-child(2)'); // Click twice to sort descending
+    
+        // Select the dropdown menu for page size and click on 10
+        await page.click('.page-size'); 
+        await page.waitForSelector('.dropdown-menu.show .dropdown-item'); // Wait for dropdown items to appear
+        await page.click('.dropdown-menu.show .dropdown-item:nth-child(2)'); // Click on the 2nd item
+    
+        // Delay 1 seconds
+        await page.evaluate(() => new Promise(resolve => setTimeout(resolve, 1000)));
+    }
+
+    // Extract specific columns from the table
+    logger.info(`Extracting table...`);
+    const tableData = await page.evaluate(() => {
+        const tableRows = Array.from(document.querySelectorAll('#table_primary tbody tr'));
+        return tableRows.map(row => {
+        const columns = row.querySelectorAll('td');
+        return {
+            type: columns[0].textContent.trim(),        // Type
+            cases: columns[1].textContent.trim(),       // Cases
+            disease: columns[2].textContent.trim(),     // Disease
+            woredaTown: columns[3].textContent.trim(),  // Woreda/Town
+            kebele: columns[4].textContent.trim()       // Kebele
+        };
+        });
+    });
+    return tableData;
+}
+
+function generateHtmlTable(tableData) {
+    const tableHtml = `
+        <table border="1" style="border-collapse: collapse; border: 1px solid #000; width: 100%; text-align: left;">
+        <thead>
+            <tr>
+                <th style="border: 1px solid #000; padding: 8px; text-align: left;">Type</th>
+                <th style="border: 1px solid #000; padding: 8px; text-align: left;">Woreda/Town</th>
+                <th style="border: 1px solid #000; padding: 8px; text-align: left;">Kebele</th>
+                <th style="border: 1px solid #000; padding: 8px; text-align: left;">Disease</th>
+                <th style="border: 1px solid #000; padding: 8px; text-align: right;">Cases</th>
+            </tr>
+        </thead>
+        <tbody>
+            ${tableData.map(row => `
+            <tr>
+                <td style="border: 1px solid #000; padding: 8px; text-align: left;">${row.type}</td>
+                <td style="border: 1px solid #000; padding: 8px; text-align: left;">${row.woredaTown}</td>
+                <td style="border: 1px solid #000; padding: 8px; text-align: left;">${row.kebele}</td>
+                <td style="border: 1px solid #000; padding: 8px; text-align: left;">${row.disease}</td>
+                <td style="border: 1px solid #000; padding: 8px; text-align: right;">${row.cases}</td>
+            </tr>
+            `).join('')}
+        </tbody>
+        </table>
+    `;
+    return tableHtml;
+}
+
+async function extractMapImage(page) {
+    const mapElement = await page.$('#mapid');
+    const mapImagePath = path.join(__dirname, 'map.png');
+    // Take a screenshot of the map
+    await mapElement.screenshot({ path: 'map.png' });
+    // Convert the map image to a base64 data URL
+    const mapImageBase64 = fs.readFileSync(mapImagePath, { encoding: 'base64' });
+    const mapImageUrl = `<img src="data:image/png;base64,${mapImageBase64}" alt="Map">`;
+    return mapImageUrl
 }
 
 async function sendEmailWithAttachments(filePath) {
