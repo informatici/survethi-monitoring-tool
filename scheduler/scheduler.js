6b480ab613d41cd50aa895619f86d2c335d1a9a8
mwithi <alessandro.domanico@yahoo.it>
2024-07-17 18:03:38 +0200

    make the scheduler to pass query parameters for puppeteer

--- scheduler/scheduler.js
+++ scheduler/scheduler.js
@@ -4,9 +4,19 @@ const winston = require('winston');
 
 require('dotenv').config();
 
-const url = 'http://puppeteer:3000/generate-pdf?url=http://tool:5000';
+const baseURL = 'http://puppeteer:3000/generate-pdf?url=http://tool:5000';
 const cronSchedule = process.env.CRON_SCHEDULE || '* * * * *'; // Default to run every minute if not set
 const logLevel = process.env.SCHEDULER_LOGLEVEL
+
+const templatePath = process.env.PUPPETEER_TEMPLATE + '.html' || "pdf_template.html";
+const diseaseFilter = process.env.REPORT_DISEASE_FILTER || "";
+const temporalFilter = process.env.REPORT_TEMPORAL_FILTER || "";
+const emailSubject = process.env.NODEMAIL_SUBJECT || 'Scheduled PDF report';
+const emailBody = process.env.NODEMAIL_BODY || 'Please find attached the scheduled PDF report.';
+
+// Construct the full URL with query parameters
+const fullURL = `${baseURL}&templatePath=${encodeURIComponent(templatePath)}&diseaseFilter=${encodeURIComponent(diseaseFilter)}&temporalFilter=${encodeURIComponent(temporalFilter)}&emailSubject=${encodeURIComponent(emailSubject)}&emailBody=${encodeURIComponent(emailBody)}`;
+
 
 // Configure logging
 const logger = winston.createLogger({
@@ -28,7 +38,7 @@ logger.info(`Scheduler started.`);
 cron.schedule(cronSchedule, async () => {
     logger.info(`Scheduler running...`);
     try {
-        const response = await axios.get(url, { timeout: 120000 }); // 120 seconds
+        const response = await axios.get(fullURL, { timeout: 120000 }); // 120 seconds
         logger.info('Scheduled task response:', response.data);
     } catch (error) {
         logger.error('Error in scheduled task:', error);
